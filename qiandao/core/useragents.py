8b3bfa47f9da53ebe0410594af38f28b52f4d293
raisons <raisonschan@gmail.com>
2024-05-16 17:49:48 +0800

    feat: 新增IT之家签到

--- qiandao/core/useragents.py
+++ qiandao/core/useragents.py
@@ -3,3 +3,8 @@
 safari = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
           "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 "
           "Safari/605.1.15")
+
+ithome = ('Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) '
+          'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 '
+          'Chrome/81.0.4044.138 Mobile Safari/537.36 '
+          'ithome/rmsdklevel2/night/7.26')
